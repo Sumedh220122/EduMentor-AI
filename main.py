37b111f4bf6fd14df6f8844820d369bfb0e47e8e
Sumedh R <132156866+Sumedh220122@users.noreply.github.com>
2025-08-05 20:31:31 +0530

    Updated mock details object for better readability

--- main.py
+++ main.py
@@ -20,12 +20,20 @@ graph_builder.add_edge("designer", END)
 
 graph = graph_builder.compile()
 
+
 details = {
   "name": "Aarav Sharma",
   "age": 19,
-  "what_are_your_interests": "I'm fascinated by robotics, artificial intelligence, and how machines can learn. I also enjoy physics and playing the guitar.",
-  "what_you_like_to_do": "I'm a curious and creative person. I love building DIY projects, reading sci-fi novels, and hiking during the weekends. I'm always looking for ways to combine tech and creativity.",
-  "how_good_are_you?": "I would say I'm intermediate. I’ve done some personal projects and taken online courses, but I still have a lot to learn.",
+  "what_are_your_interests": """
+      I'm fascinated by robotics, artificial intelligence, and how machines can learn. I also enjoy physics and playing the guitar.
+    """,
+  "what_you_like_to_do": """
+      I'm a curious and creative person. 
+      I love building DIY projects, reading sci-fi novels, and hiking during the weekends. 
+      I'm always looking for ways to combine tech and creativity.
+    """,
+  "how_good_are_you?": "I would say I'm intermediate. "
+    "I’ve done some personal projects and taken online courses, but I still have a lot to learn.",
   "how_long_are_you_willing_to_commit_to_learning": "Around 6-8 hours a week consistently, more during vacations."
 }
 
@@ -34,3 +42,4 @@ output = graph.invoke({"messages": [{"role" : "user", "content" : json.dumps(det
 with open("student_roadmap.json", "w") as f:
     roadmap : StudentRoadMaps = output["roadmap"]
     json.dump(roadmap.model_dump(), f, indent=4)
+
